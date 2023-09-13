fc7ce45e4743ecac4841d347098138d515359f72
diliupg <diliupg@gmail.com>
2023-09-13 22:51:32 +0530

    initial commit. Setup the basic movement controller with Cinemachine

---
+++ Assets/Scripts/MovementStateManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementStateManager : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    [HideInInspector] public Vector3 dir;
+    float hzInput, vInput;
+    CharacterController controller;
+
+    [SerializeField] float groundYOffset;
+    [SerializeField] LayerMask groundMask;
+    Vector3 spherePos;
+
+    [SerializeField] float gravity = -9.81f;
+    Vector3 velocity;
+
+    [SerializeField] private Color color = Color.red;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        GetDirectionAndMove();
+        Gravity();
+    }
+
+    void GetDirectionAndMove()
+    {
+        hzInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        dir = transform.forward * vInput + transform.right * hzInput;
+
+        controller.Move(dir * moveSpeed * Time.deltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        spherePos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        if (Physics.CheckSphere(spherePos, controller.radius - 0.05f, groundMask)) return true;
+        return false;
+    }
+
+    void Gravity()
+    {
+        if (!IsGrounded()) velocity.y += gravity * Time.deltaTime;
+        else if (velocity.y < 0 ) velocity.y = -2f;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(spherePos, controller.radius - 0.05f); 
+    }
+}
